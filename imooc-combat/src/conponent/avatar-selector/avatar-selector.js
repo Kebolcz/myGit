3386ce15ba86cb1c26424d87dcfe053bae73b05b
刘崇桢 <liucz1@guahao.com>
2018-09-12 09:37:16 +0800

    20180911_git_liucz1

---
+++ imooc-combat/src/conponent/avatar-selector/avatar-selector.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class AvatarSelector extends React.Component {
+  render() {
+    return '头像选择'
+  }
+}
+
+export default AvatarSelector;
--- imooc-combat/src/container/bossinfo/bossinfo.js
+++ imooc-combat/src/container/bossinfo/bossinfo.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import { NavBar, Icon, InputItem } from 'antd-mobile';
+import AvatarSelector from '../../conponent/avatar-selector/avatar-selector';
 
 class BossInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: ''
+    }
+  }
+
+  onChange(key, val) {
+    this.setState({
+      [key]: val
+    })
+  }
+
   render() {
-    return <h2>bossinfo</h2>
+    return (
+    <div>
+      <NavBar mode="light"
+      icon={<Icon type="left" />}
+      onLeftClick={() => console.log('onLeftClick')}
+      >BossInfo</NavBar>
+      <AvatarSelector></AvatarSelector>
+      <InputItem onChange={(v)=>{this.onChange('title',v)}}>招聘职位</InputItem>
+      <InputItem onChange={(v)=>{this.onChange('title',v)}}>公司名称</InputItem>
+      <InputItem onChange={(v)=>{this.onChange('title',v)}}>职位薪资</InputItem>
+      <InputItem onChange={(v)=>{this.onChange('title',v)}}>职位要求</InputItem>
+    </div>
+    )
   }
 }
 
